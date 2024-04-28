b3d73416c8e85688fc56c78fdbdf9be0ad31d19d
Marcelo Torres <marcelotorres1982@gmail.com>
2024-04-28 10:32:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-'''
-Para nos auxiliar neste pequeno projeto, vamos contar com a ajuda da função choice() da biblioteca random,
-ela será responsável por emular uma escolha pseudo-aleatória da máquina, entre as três opções disponíveis
-(Pedra, Papel e Tesoura).
-
-Também iremos utilizar um While Loop para manter o jogo executando até que o usuário deseje sair.
-
-Coletaremos o input do usuário através da função input() e testaremos uma série de if, elif e else para sabermos quem
-é o vitorioso.
-'''
-
 from random import choice
 
 opcoes = ['pedra', 'papel', 'tesoura']
